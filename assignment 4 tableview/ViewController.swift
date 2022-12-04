f1f18e8a7bef6470e5d5e45b3e1b58b5468f6d9e
navjotn406 <113278709+navjotn406@users.noreply.github.com>
2022-12-04 18:33:14 -0500

    Initial Commit

---
+++ assignment 4 tableview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment 4 tableview
+//
+//  Created by Navjot Singh on 2022-12-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

4501f6b8f520fe9d80daf16d7503c3540695cb9e
navjotn406 <113278709+navjotn406@users.noreply.github.com>
2022-12-05 22:10:36 -0500

    added add and delete row functionality

--- assignment 4 tableview/ViewController.swift
+++ assignment 4 tableview/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var txtInput: UITextField!
+    @IBOutlet weak var tableview: UITableView!
+    
+    var strArr = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func onclickAddBtn(_ sender: Any) {
+        if let text = txtInput.text, !text.isEmpty{
+            self.strArr.insert(text, at: 0)
+            tableview.beginUpdates()
+            tableview.insertRows(at: [IndexPath(row: 0, section: 0)], with: .right)
+            tableview.endUpdates()
+            txtInput.text = nil
+        }
+    }
+    
+}
+
+extension ViewController:UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return strArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "editTableViewCell", for: indexPath) as? editTableViewCell else {return UITableViewCell()}
+        cell.textLabel?.text = txtInput.text
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            strArr.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .none)
+        }
+    }
 
 }
+
+
+
 
---
+++ assignment 4 tableview/editTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  editTableViewCell.swift
+//  assignment 4 tableview
+//
+//  Created by Navjot Singh on 2022-12-04.
+//
+
+import UIKit
+
+class editTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
